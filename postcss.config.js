edab4a1a9232a82659d1f065e3b53c29a3d9fb8e
Miguel Angel <devmiguelangel@gmail.com>
2018-10-09 17:34:02 -0400

    custom media queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   plugins: {
     'postcss-preset-env': {
-      browsers: 'last 3 versions',
-      stage: 3,
       features: {
         autoprefixer: {
           grid: true,
           flexbox: false,
         },
+        'custom-media-queries': {},
       },
     },
     /* autoprefixer: {
